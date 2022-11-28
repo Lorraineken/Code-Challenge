d91dedae169544f241d9539d9e13052bc12176f1
Lorraineken <lorrainekupa@gmail.com>
2022-11-28 12:20:20 +0300

    updated speedDetector to include License suspension

--- speedDetector.js
+++ speedDetector.js
@@ -1,7 +1,9 @@
 /*Write a program that takes as input the speed of a car e.g 80. 
  If the speed is less than 70, it should print “Ok”.
  Otherwise, for every 5 km/s above the speed limit (70), 
- it should give the driver one demerit point and print the total number of demerit points.*/
+ it should give the driver one demerit point and print the total number of demerit points.
+ If the driver gets more than 12 points, the function should print: “License suspended”.
+ */
 
 let demeritPoint = 0;
 
@@ -11,8 +13,13 @@ function speedDetector(speed){
     }
     else{
         demeritPoint = (speed-70)/5;
+        if(demeritPoint>12){
+            console.log("License suspended")
+        }
+        else{
         console.log(`demerit points: ${demeritPoint}`)
+        }
     }
 }
 
-speedDetector(66)
+speedDetector(140)
